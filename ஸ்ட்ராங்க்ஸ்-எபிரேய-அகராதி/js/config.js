b29b89fbc93503080cac78b0983b34419c01e8f0
Yesudas S <yesudas.rs@gmail.com>
2025-09-01 19:53:29 +0530

    Added new dictionary Strongs Hebrew in Tamil

---
+++ ஸ்ட்ராங்க்ஸ்-எபிரேய-அகராதி/js/config.js
@@ -0,0 +1,6 @@
+$SITE_ROOT = 'feedback';
+$SITE_BASE_PATH = 'https://wordofgod.in/';
+$SHOW_DEBUG_ALERTS = false;
+$APP_NAME = "ஸ்ட்ராங்க்ஸ் எபிரேய அகராதி";
+$APP_DESCRIPTION = "வேதாகமத்தில் உள்ள எபிரேய வார்த்தைகளுக்கு பண்டிதர் ஜேம்ஸ் ஸ்ட்ராங்க்ஸ் எழுதின அகராதி from www.wordofgod.in";
+$APP_WEB_URL = "https://wordofgod.in/dictionary/ஸ்ட்ராங்க்ஸ்-எபிரேய-அகராதி";
---
+++ ஸ்ட்ராங்க்ஸ்-எபிரேய-அகராதி/sitemap.php
@@ -0,0 +1,46 @@
+<?php
+// sitemap.php
+
+// Path to Dictionary.json
+$jsonFile = __DIR__ . '/data/Dictionary.json';
+$sitemapFile = __DIR__ . '/sitemap.xml';
+
+// Read and decode JSON
+$json = file_get_contents($jsonFile);
+if ($json === false) {
+    die('Could not read Dictionary.json');
+}
+$data = json_decode($json, true);
+if (!$data || !isset($data['words'])) {
+    die('Invalid Dictionary.json');
+}
+
+// XML header
+$xml = '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL;
+$xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EOL;
+
+// Base URL
+$baseUrl = 'http://wordofgod.in/bibledictionary/ஸ்ட்ராங்க்ஸ்-எபிரேய-அகராதி/#/';
+
+// Today's date in YYYY-MM-DD format
+$today = date('Y-m-d');
+
+// Add each word as a URL
+foreach ($data['words'] as $word) {
+    // URL encode the word for the link
+    $wordUrl = rawurlencode($word['word']);
+    $loc = $baseUrl . $wordUrl . '/' . $word['id'];
+    $xml .= "  <url>\n";
+    $xml .= "    <loc>{$loc}</loc>\n";
+    $xml .= "    <lastmod>{$today}</lastmod>\n";
+    $xml .= "  </url>\n";
+}
+
+$xml .= '</urlset>';
+
+// Write to sitemap.xml
+file_put_contents($sitemapFile, $xml);
+
+// Output success message
+echo json_encode(['success' => true, 'message' => 'sitemap.xml created', 'count' => count($data['words'])]);
+?>
